56bbc73ef9e0732b447aa6c13626dbc64c0db758
miyabi-5796 <yamakei0916.5796@gmail.com>
2026-04-06 18:54:02 +0900

    天気予報情報取得APIの実装。　現状東京のみ取得可能で、任意の地名を入力して取得するには別のAPIを実装する必要がある

--- Weather_App.ts
+++ Weather_App.ts
@@ -1,1 +1,87 @@
-console.log("Hello");
+class WeatherInfo {
+  #day: string = "";
+  #temp_min: number = -999;
+  #temp_max: number = -999;
+  #prec_max: number = -999;
+
+  constructor(
+    day: string,
+    temp_min: number,
+    temp_max: number,
+    prec_max: number,
+  ) {
+    this.#day = day;
+    this.#temp_min = temp_min;
+    this.#temp_max = temp_max;
+    this.#prec_max = prec_max;
+  }
+
+  output(): string {
+    return `date: ${this.#day}, 最低気温: ${this.#temp_min}, 最高気温: ${this.#temp_max}, 降水確立: ${this.#prec_max}`;
+  }
+}
+
+class WeatherInfoManager {
+  #week: WeatherInfo[] = [];
+  constructor() {}
+
+  add_weather_info(
+    day: string,
+    temp_min: number,
+    temp_max: number,
+    prec_max: number,
+  ) {
+    this.#week.push(new WeatherInfo(day, temp_min, temp_max, prec_max));
+  }
+
+  view_weather_info_in_week() {
+    for (var info of this.#week) {
+      console.log(info.output());
+    }
+  }
+}
+
+class WeatherApi {
+  constructor() {}
+  async get_weather(url: string) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Http Error. Status : ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (!data) {
+        throw new Error(`Failed to Recieved Json Data`);
+      }
+
+      const weather_manager = new WeatherInfoManager();
+
+      var index = 0;
+      var day = data["daily"];
+      for (var time of day["time"]) {
+        const temp_mins = day["temperature_2m_min"];
+        const temp_maxs = day["temperature_2m_max"];
+        const prec_maxs = day["precipitation_probability_max"];
+        weather_manager.add_weather_info(
+          time,
+          temp_mins[index],
+          temp_maxs[index],
+          prec_maxs[index],
+        );
+        index++;
+      }
+
+      weather_manager.view_weather_info_in_week();
+    } catch (error) {
+      console.error("Connection Error : ", error);
+    }
+  }
+}
+
+const api = new WeatherApi();
+
+api.get_weather(
+  "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=temperature_2m_min,temperature_2m_max,precipitation_probability_max&hourly=temperature_2m,precipitation_probability&current=temperature_2m,precipitation&timezone=Asia%2FTokyo",
+);
